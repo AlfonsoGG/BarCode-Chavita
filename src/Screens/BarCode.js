2678080b98c60fd28a02a74d31955a05eb30ac6a
AlfonsoGG <31867012+AlfonsoGG@users.noreply.github.com>
2021-07-15 21:37:12 -0500

    Actualizacion/Api

--- src/Screens/BarCode.js
+++ src/Screens/BarCode.js
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from "react";
-import { Text, View, StyleSheet, Button,  TouchableOpacity } from "react-native";
+import { Text, View, StyleSheet, Button, TouchableOpacity } from "react-native";
 import { BarCodeScanner } from "expo-barcode-scanner";
 import { SafeAreaView } from "react-native";
 import { FontAwesome } from "@expo/vector-icons";
+import Toast from "react-native-simple-toast";
 
 export default function App() {
   const [hasPermission, setHasPermission] = useState(null);
@@ -15,11 +16,6 @@ export default function App() {
     })();
   }, []);
 
-  const handleBarCodeScanned = ({ type, data }) => {
-    setScanned(true);
-    alert(`Bar code with type ${type} and data ${data} has been scanned!`);
-  };
-
   if (hasPermission === null) {
     return <Text>Requesting for camera permission</Text>;
   }
@@ -27,20 +23,50 @@ export default function App() {
     return <Text>No access to camera</Text>;
   }
 
+  const Scanner = ({ type, data }) => {
+    setScanned(true);
+    apiConsult(data);
+  };
+
+  let apiConsult = async (data) => {
+    console.log(data);
+    url = "http://26ca6770d402.ngrok.io/productos"
+    const requestOptions = {
+      method: "POST",
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: data,
+    };
+
+    await fetch(url, requestOptions)
+      .then((response) => response.json())
+      .then((response) => {
+        console.log(response)
+      })
+      .catch((error) => {
+        //this.setState({ errorMessage: error.toString() });
+        console.log(error)
+      });
+  };
+
   return (
     <SafeAreaView style={styles.container}>
-         <View style={{flex: 1}}>
+      <View style={{ flex: 1 }}>
         <BarCodeScanner
-          onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
+          onBarCodeScanned={scanned ? undefined : Scanner}
           style={StyleSheet.absoluteFillObject}
         />
       </View>
       {scanned && (
-          <TouchableOpacity style={styles.button} onPress={() => setScanned(false)}>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => setScanned(false)}
+        >
           <FontAwesome name="camera" size={23} color="#FFF" />
-          </TouchableOpacity>
-          
-        )}
+        </TouchableOpacity>
+      )}
     </SafeAreaView>
   );
 }
@@ -57,6 +83,5 @@ const styles = StyleSheet.create({
     margin: 30,
     borderRadius: 10,
     height: 50,
-    
   },
 });
